4394e1c014a5803fe66f624d62ab24e8d9cf69ce
samuelesimone <samuelesimone01@gmail.com>
2025-01-02 18:29:09 +0100

    Removing opening new tab external links

--- _plugins/open_external_links_in_new_tab.rb
+++ _plugins/open_external_links_in_new_tab.rb
@@ -21,7 +21,7 @@ def convert_links(doc)
   if open_external_links_in_new_tab
     parsed_doc = Nokogiri::HTML::DocumentFragment.parse(doc.content)
     parsed_doc.css("a:not(.internal-link):not(.footnote):not(.reversefootnote)").each do |link|
-      link.set_attribute('target', '_blank')
+      #link.set_attribute('target', '_blank')
     end
     doc.content = parsed_doc.inner_html
   end
